5d6116e41365baf0e0fbd45a9d00ca4104b2edfe
Olivier Butler <olivier.butler@gmail.com>
2017-09-07 15:14:48 -0700

    Answer button now does things.

--- iOS Trivia/ViewController.swift
+++ iOS Trivia/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func answerButton(_ sender: UIButton) {
+        print("Starting speech")
+        let synth = AVSpeechSynthesizer()
+        let podium = "Someone pressed my answer button."
+        let spookyVoice = AVSpeechUtterance(string: podium)
+        spookyVoice.rate = 0.5
+        synth.speak(spookyVoice)
+        print("Spoken")
+    }
+    @IBAction func nextButton(_ sender: UIButton) {
+        print("Next please")
+    }
+    @IBAction func prevButton(_ sender: UIButton) {
+        print("Please upgrade to Tinder Plus to undo your last swipe")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = "What do you call the space underneath a kettle?"
+        answerLabel.text = "I don't know, it doesn't have a name. I just want to sleep, why can't we sleep at the same times??? Now that's a good question."
     }
 
     override func didReceiveMemoryWarning() {
