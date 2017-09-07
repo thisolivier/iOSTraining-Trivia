6bc2d19dc5669b553a639acb35977d0841fe420f
Olivier Butler <olivier.butler@gmail.com>
2017-09-06 20:18:27 -0700

    Initial Commit

---
+++ iOS Trivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Trivia
+//
+//  Created by Olivier Butler on 06/09/2017.
+//  Copyright © 2017 Olivier Butler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
